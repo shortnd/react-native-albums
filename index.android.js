6f5c8f3be203bcd3202fc4b8efb0fe4555642602
Collin OConnell <ococncol@gmail.com>
2017-05-16 11:45:29 -0400

    imported React and React-native

---
+++ index.android.js
@@ -0,0 +1,10 @@
+// Index.android.js - Code goes in here for ANDROID
+
+// Import a library to help create a component
+import React from 'react';
+import ReactNative from 'react-native';
+
+//Create a component
+
+
+//Render it to the device
---
+++ index.ios.js
@@ -0,0 +1,1 @@
+// Index.ios.js - place code here for IOS
