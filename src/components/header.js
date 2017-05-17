41e762da01915752793fa4235d496069d1654c4c
Collin OConnell <ococncol@gmail.com>
2017-05-17 12:29:10 -0400

    added more styling to header component

--- src/components/header.js
+++ src/components/header.js
@@ -17,7 +17,23 @@ const Header = () => {
 const styles = {
   //For the view
   viewStyle: {
-    backgroundColor: '#F8F8F8'    
+    backgroundColor: '#F8F8F8',
+    //Already flexbox so just have to move with
+    //justifyContent and alignItems
+    justifyContent: 'center',
+    alignItems: 'center',
+    //Set the height of the header to 60
+    height: 60,
+    //Gave it paddingTop of 15
+    paddingTop: 15,
+    //Creating border with a shadow
+    shadowColor: '#000',
+    //Width means left/right hight is up/down
+    shadowOffset: { width: 0, height: 2 },
+    //Changes how hard the color is between 0-1
+    shadowOpacity: 0.5,
+    elevation: 2,
+    position: 'relative'
   },
   //For the text
   textStyle: {
